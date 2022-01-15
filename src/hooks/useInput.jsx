5c86300e03052546919fc6c8fddeb57905f688bf
chango <kimtothechang@gmail.com>
2022-01-16 03:54:23 +0900

    :memo: Study useInput

---
+++ src/hooks/useInput.jsx
@@ -0,0 +1,38 @@
+import React, { StrictMode, useState } from "react";
+import ReactDOM from "react-dom";
+
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
+const App = () => {
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("Mr. ", maxLen);
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  rootElement
+);
